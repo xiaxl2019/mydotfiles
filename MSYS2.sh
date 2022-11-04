eab97dbdd580e51f706c183d5b9f44e495a3bb3e
xiaxl2019 <xiaxl@fudan.edu.cn>
2022-11-04 22:08:05 +0800

    change MSYS2.sh: add fzf, remove python things

--- MSYS2.sh
+++ MSYS2.sh
@@ -6,11 +6,6 @@ echo "----------------------"
 pacman -S --needed base-devel mingw-w64-x86_64-toolchain
 pacman -S --needed cmake
 
-echo "-------------------------"
-echo "Install python things ..."
-echo "-------------------------"
-pacman -S --needed mingw-w64-x86_64-python-{numpy,scipy,matplotlib}
-
 echo "----------------------"
 echo "Install vim things ..."
 echo "----------------------"
@@ -22,5 +17,6 @@ echo "Install tools ..."
 echo "-----------------"
 pacman -S --needed git
 pacman -S --needed man-pages-posix
+pacman -S --needed mingw-w64-x86_64-fzf
 pacman -S --needed rsync
 pacman -S --needed tree
