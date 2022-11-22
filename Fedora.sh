e41a466dd9a3fdcf76230435d18019f618872f4a
xiaxl2019 <xiaxl@fudan.edu.cn>
2022-11-22 22:27:29 +0800

    change Fedora.sh

--- Fedora.sh
+++ Fedora.sh
@@ -42,19 +42,16 @@ echo "-----------------------"
 excute "install vim-enhanced"
 excute "install gcc gcc-c++ gcc-gfortran"
 excute "install make cmake"
-excute "install python3-numpy python3-scipy python3-matplotlib python3-sympy"
+excute "install python3-numpy python3-scipy python3-matplotlib"
 
 for i in numpy scipy matplotlib sympy ; do python3 -c "import $i; print($i.__version__, $i)"; done
 python3 -c "import matplotlib.pyplot as plt; plt.text(0.5, 0.5, '$\sin \pi x$'); plt.show()"
 echo ;
 
-excute "install numba" \
-    "pip install numba -i https://pypi.tuna.tsinghua.edu.cn/simple"
-
 # excute "install gsl gsl-devel"
 # excute "install openmpi openmpi-devel"
 # excute "install slurm-slurmctld slurm-slurmd slurm-doc"
-# excute "install root"
+excute "install root"
 
 excute "install expat-devel libX11-devel libXmu-devel qt5-qt3d-devel"
 excute "install tealdeer fzf autojump htop glances duf"
@@ -65,9 +62,8 @@ echo "Install Latex things ..."
 echo "------------------------"
 
 excute "install lyx"
-# excute "install gnome-latex"
-# excute "install texstudio"
-# excute "install texlive-revtex texlive-elsarticle"
+excute "install latexmk"
+excute "install texlive-revtex texlive-elsarticle"
 excute "install texlive-ctex"
 excute "install texlive-latexdiff texlive-latexindent"
 excute "install texlive-newtx"
@@ -79,14 +75,10 @@ echo "Other things ..."
 echo "----------------"
 
 excute "install libreoffice-langpack-zh-Hans"
-# excute "install asymptote"
-# excute "install filezilla"
-# excute "install inkscape"
 excute "install keepassx"
 excute "install gnome-tweaks"
 excute "install meld"
 excute "install pandoc"
 excute "install papirus-icon-theme"
 excute "install evolution"
-# excute "install thunderbird"
 excute "install vlc"
